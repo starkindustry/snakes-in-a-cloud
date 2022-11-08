cd2a2259c627b17283658ac7e717e4e3ff6bab15
starkindustry <kevostarks@gmail.com>
2022-11-08 08:07:16 -0800

    add Variable tof subnet_azs

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   type        = string
   description = "The region the environment will be launched within"
 }
+
+variable "subnet_az" {
+  type        = list(string)
+  description = "AZs for the public subnets"
+}
